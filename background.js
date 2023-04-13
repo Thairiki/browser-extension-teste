20f37c0a42a3513e463b78f60ca9b895611d8fb9
Thairiki <thairiki2012@gmail.com>
2023-04-13 15:59:24 -0400

    a

--- background.js
+++ background.js
@@ -1,39 +1,46 @@
 const API = chrome || browser
 
-API.runtime.onInstalled.addListener(() => {
-  API.action.setBadgeText({
-    text: "OFF",
-  });
-});
+document.querySelectorAll('div.css-w94bws')
+  .forEach(e => {
+    const btn = document.createElement('button')
+    btn.innerHTML = 'Chamar'
+    e.appendChild(btn)
+  })
 
-const extensions = 'https://developer.chrome.com/docs/extensions'
-const webstore = 'https://developer.chrome.com/docs/webstore'
+// API.runtime.onInstalled.addListener(() => {
+//   API.action.setBadgeText({
+//     text: "OFF",
+//   });
+// });
 
-API.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-    const prevState = await API.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON'
+// const extensions = 'https://developer.chrome.com/docs/extensions'
+// const webstore = 'https://developer.chrome.com/docs/webstore'
 
-    // Set the action badge to the next state
-    await API.action.setBadgeText({
-      tabId: tab.id,
-      text: nextState,
-    })
+// API.action.onClicked.addListener(async (tab) => {
+//   if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
+//     // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+//     const prevState = await API.action.getBadgeText({ tabId: tab.id });
+//     // Next state will always be the opposite
+//     const nextState = prevState === 'ON' ? 'OFF' : 'ON'
 
-    if (nextState === "ON") {
-      // Insert the CSS file when the user turns the extension on
-      await API.scripting.insertCSS({
-        files: ["./css/focus-mode.css"],
-        target: { tabId: tab.id },
-      });
-    } else if (nextState === "OFF") {
-      // Remove the CSS file when the user turns the extension off
-      await API.scripting.removeCSS({
-        files: ["./css/focus-mode.css"],
-        target: { tabId: tab.id },
-      });
-    }
-  }
-})
+//     // Set the action badge to the next state
+//     await API.action.setBadgeText({
+//       tabId: tab.id,
+//       text: nextState,
+//     })
+
+//     if (nextState === "ON") {
+//       // Insert the CSS file when the user turns the extension on
+//       await API.scripting.insertCSS({
+//         files: ["./css/focus-mode.css"],
+//         target: { tabId: tab.id },
+//       });
+//     } else if (nextState === "OFF") {
+//       // Remove the CSS file when the user turns the extension off
+//       await API.scripting.removeCSS({
+//         files: ["./css/focus-mode.css"],
+//         target: { tabId: tab.id },
+//       });
+//     }
+//   }
+// })
