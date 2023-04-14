1b145c3371822c9ce8bf18a6395e45f80327826a
Thairiki <thairiki2012@gmail.com>
2023-04-14 14:47:25 -0400

    aaaaaaaaaaaaaaaa

--- background.js
+++ background.js
@@ -1,19 +1,5 @@
 const API = chrome || browser
 
-// API.runtime.onInstalled.addListener(async tab => {
-//   console.log(tab)
-//   await API.scripting
-//     .registerContentScripts([{
-//       id: "session-script",
-//       js: ["/js/injectButton.js"],
-//       persistAcrossSessions: false,
-//       matches: ["<all_urls>"],
-//       runAt: "document_idle",
-//     }])
-//     .then(() => console.log("registration complete"))
-//     .catch(err => console.error(`failed to register content scripts: ${err}`))
-// })
-
 API.tabs.onUpdated.addListener( async (tabId, changeInfo, tab) => {
   if (changeInfo.status == 'complete' && tab.active) {
     await API.scripting
@@ -25,54 +11,3 @@ API.tabs.onUpdated.addListener( async (tabId, changeInfo, tab) => {
     .catch(err => console.error(`failed to inject content scripts: ${err}`))
   }
 })
-
-// API.action.onClicked.addListener(async () => {
-//   await API.scripting
-//     .registerContentScripts([{
-//       id: "session-script",
-//       js: ["/js/injectButton.js"],
-//       persistAcrossSessions: false,
-//       matches: ["<all_urls>"],
-//       runAt: "document_start",
-//     }])
-//     .then(() => console.log("registration complete"))
-//     .catch(err => console.error(`failed to register content scripts: ${err}`))
-// })
-
-// API.runtime.onInstalled.addListener(() => {
-//   API.action.setBadgeText({
-//     text: "OFF",
-//   });
-// });
-
-// const extensions = 'https://developer.chrome.com/docs/extensions'
-// const webstore = 'https://developer.chrome.com/docs/webstore'
-
-// API.action.onClicked.addListener(async (tab) => {
-//   if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
-//     // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
-//     const prevState = await API.action.getBadgeText({ tabId: tab.id });
-//     // Next state will always be the opposite
-//     const nextState = prevState === 'ON' ? 'OFF' : 'ON'
-
-//     // Set the action badge to the next state
-//     await API.action.setBadgeText({
-//       tabId: tab.id,
-//       text: nextState,
-//     })
-
-//     if (nextState === "ON") {
-//       // Insert the CSS file when the user turns the extension on
-//       await API.scripting.insertCSS({
-//         files: ["./css/focus-mode.css"],
-//         target: { tabId: tab.id },
-//       });
-//     } else if (nextState === "OFF") {
-//       // Remove the CSS file when the user turns the extension off
-//       await API.scripting.removeCSS({
-//         files: ["./css/focus-mode.css"],
-//         target: { tabId: tab.id },
-//       });
-//     }
-//   }
-// })
--- js/injectButton.js
+++ js/injectButton.js
@@ -1,12 +1,15 @@
   console.log('injecting buttons')
   debugger
-  const divs = document.querySelectorAll('div.css-w94bws')
   
-  divs.forEach(e => {
-    let i = 0
-    const btn = document.createElement('button')
-    btn.innerHTML = 'Chamar'
-    e.appendChild(btn)
-    console.log(i)
-    i++
-  })
+  setTimeout(() => {
+    const divs = document.querySelectorAll('div.css-w94bws')
+    
+    divs.forEach(e => {
+      let i = 0
+      const btn = document.createElement('button')
+      btn.innerHTML = 'Chamar'
+      e.appendChild(btn)
+      console.log(i)
+      i++
+    })
+  }, 3000)
