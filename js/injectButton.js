cd106905d46b901d4273de87634789e1caa5c1e5
Thairiki <thairiki2012@gmail.com>
2023-04-17 09:07:12 -0400

    a

--- js/injectButton.js
+++ js/injectButton.js
@@ -4,8 +4,8 @@
     const items = document.querySelectorAll('div.css-50kdbq')
 
     items.forEach(item => {
-      const nome = item.querySelector('css-11zrb1w')
-      const localBotao = item.querySelector('css-w94bws')
+      const nome = item.querySelector('div.css-11zrb1w')
+      const localBotao = item.querySelector('div.css-w94bws')
       const btn = document.createElement('button')
       btn.innerHTML = 'Chamar'
       btn.addEventListener('click', () => {
