57a62b4b3637e5c176120abd60895bd0925c51fb
Thairiki <thairiki2012@gmail.com>
2023-04-17 08:35:07 -0400

    test

--- js/injectButton.js
+++ js/injectButton.js
@@ -1,7 +1,11 @@
   console.log('injecting buttons')
   
   setTimeout(() => {
+    const test = document.querySelectorAll('css-50kdbq')
     const divs = document.querySelectorAll('div.css-w94bws')
+    const nomes = document.querySelectorAll('css-11zrb1w')
+
+    console.log(test)
     
     divs.forEach(e => {
       const btn = document.createElement('button')
