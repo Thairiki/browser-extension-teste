d12f46e9aad5e6443c9b67322a2c21d134038d08
Thairiki <thairiki2012@gmail.com>
2023-04-17 08:27:25 -0400

    a

--- js/injectButton.js
+++ js/injectButton.js
@@ -1,15 +1,11 @@
   console.log('injecting buttons')
-  debugger
   
   setTimeout(() => {
     const divs = document.querySelectorAll('div.css-w94bws')
     
     divs.forEach(e => {
-      let i = 0
       const btn = document.createElement('button')
       btn.innerHTML = 'Chamar'
       e.appendChild(btn)
-      console.log(i)
-      i++
     })
   }, 3000)
